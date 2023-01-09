b619a6f7abf21479b6c041cc1b71763c2f6c8278
urswilke <urs.wilke@gmail.com>
2023-01-09 22:53:22 +0100

    - setup strudel as described here: https://github.com/tidalcycles/strudel/tree/main/my-patterns#my-patterns
    - add first strudel pattern

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -30,8 +30,8 @@ export default defineConfig({
     mdx(options),
     tailwind(),
   ],
-  site: `https://strudel.tidalcycles.org`,
-  base: '/',
+  site: `urswilke.github.io`,
+  base: '/strudel',
 });
 
 /*
